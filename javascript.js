b60688c4542a0b47d87d116f8b9c8652f3db62b3
Spencer Kifell <49565358+Spencerkifell@users.noreply.github.com>
2020-10-29 00:10:46 -0400

    Features Development (Playing Around with APIs)

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ function showalert()
 {
     alert("Thank you for submitting your inquiry.\nI will be in contact with you shortly.\nI look forward to speaking with you\n- Spencer Kifell")
 }
+
+document.getElementById("myBtnApi").addEventListener("click", function() {
+    console.log("hello");
+}); 
