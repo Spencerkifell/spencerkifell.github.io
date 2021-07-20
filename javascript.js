8854f3bd83cda37e320897a55700ff6c81c9218a
Spencer <spencerkfl@gmail.com>
2021-07-19 22:37:01 -0400

    Update

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function showalert()
-{
-    alert("Thank you for submitting your inquiry.\nI will be in contact with you shortly.\nI look forward to speaking with you\n- Spencer Kifell")
-}
-
-document.getElementById("myBtnApi").addEventListener("click", function() {
-    console.log("hello");
-}); 
